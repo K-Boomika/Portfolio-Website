709c29b6ca1d17fc948c2920d5499ec2be8ad01f
Boomika-Karuppaiah <boomika_k@gwmail.gwu.edu>
2024-06-10 22:53:28 -0400

    Updated contact feature and skills
    Added backend feature to send email and update skills look.

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -9,20 +9,23 @@ function Skills() {
         <div className={styles.skillList}>
             <SkillList src={checkMarkIcon} skill="C++" />
             <SkillList src={checkMarkIcon} skill="Python" />
+            <SkillList src={checkMarkIcon} skill="C#" />
+            <SkillList src={checkMarkIcon} skill="SQL" />
+            <SkillList src={checkMarkIcon} skill="Kotlin" />
             <SkillList src={checkMarkIcon} skill="Java" />
+        </div>
+        <hr />
+        <div className={styles.skillList}>
             <SkillList src={checkMarkIcon} skill="HTML" />
             <SkillList src={checkMarkIcon} skill="CSS" />
             <SkillList src={checkMarkIcon} skill="JavaScript" />
             <SkillList src={checkMarkIcon} skill="Node" />
-            <SkillList src={checkMarkIcon} skill="SQL" />
-            <SkillList src={checkMarkIcon} skill="Kotlin" />
-            <SkillList src={checkMarkIcon} skill="C#" />
         </div>
         <hr />
         <div className={styles.skillList}>
+            <SkillList src={checkMarkIcon} skill="Vue" />
             <SkillList src={checkMarkIcon} skill="React" />
             <SkillList src={checkMarkIcon} skill="Angular" />
-            <SkillList src={checkMarkIcon} skill="Vue" />
         </div>
         <hr />
         <div className={styles.skillList}>
